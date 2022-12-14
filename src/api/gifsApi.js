5d939fbbd096aad73b5b0f91e9e5038ac02d8864
fernando flores <martinjanns2000@gmail.com>
2022-12-14 07:41:52 -0600

    se reparo el error aldeplegar a netlify

--- src/api/gifsApi.js
+++ src/api/gifsApi.js
@@ -1,11 +1,8 @@
 import axios from "axios";
-import { config } from "../helpers/config";
-
-const { VITE_APP_URL_API } = config()
 
 
 export const gifsApi = axios.create({
-    baseURL: VITE_APP_URL_API
+    baseURL: import.meta.env.VITE_APP_URL_API
 })
 
 
